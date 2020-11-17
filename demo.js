7e9ed3e91c8e18f4bf40011cff75b8bbd33505d1
Jerry9923 <402031271@qq.com>
2020-11-17 11:47:24 +0800

    修改文件

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@
 window.onload = function (){
     let box = document.getElementsByClassName('box')[0];
     box.addListener('click',()=>{
-        alert('这是测试代码');
+        alert('这是测试de 代码');
     })
 }
 
